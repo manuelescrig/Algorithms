0d3d94632c4b46c98d03e064dbd5bc1c84e23b93
Manuel Escrig Ventura <manuelescrig@gmail.com>
2017-01-16 13:20:17 +0100

    Added problems about traversing trees.

---
+++ Java/DynamicProgramming/MatrixMaxSumRectangle.java
@@ -0,0 +1,98 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/*
+Given a non-empty 2D matrix matrix and an integer k,
+find the max sum of a rectangle in the matrix such that its sum is no larger
+than k.
+
+Example:
+Given matrix = [
+  [1,  0, 1],
+  [0, -2, 3]
+]
+k = 2
+The answer is 2. Because the sum of rectangle [[0, 1], [-2, 3]] is 2 and 2
+is the max number no larger than k (k = 2).
+
+// https://www.quora.com/Given-an-array-of-integers-A-and-an-integer-k-find-a-subarray-that-contains-the-largest-sum-subject-to-a-constraint-that-the-sum-is-less-than-k
+// https://www.youtube.com/watch?v=yCQN096CwWM
+
+*/
+
+class MatrixMaxSumRectangle {
+
+  public static void print(int[][] array) {
+    for (int row=0; row<array.length; row++) {
+      for (int col=0; col<array[0].length; col++) {
+        System.out.print(array[row][col] + " ");
+      }
+      System.out.println("");
+    }
+  }
+
+  public static int maxSumSubmatrix(int[][] matrix, int k) {
+      // 2D Kadane's algorithm + 1D maxSum problem with sum limit k
+      // 2D subarray sum solution
+
+      // Boundary check
+      if(matrix.length == 0) return 0;
+
+      int m = matrix.length, n = matrix[0].length;
+      int result = Integer.MIN_VALUE;
+
+      // Outer loop should use smaller axis
+      // Now we assume we have more rows than cols, therefore outer loop will be based on cols
+      for (int left = 0; left < n; left++) {
+          // Array that accumulate sums for each row from left to right
+          int[] sums = new int[m];
+          for (int right = left; right < n; right++){
+              // Update sums[] to include values in curr right col
+              for(int i = 0; i < m; i++){
+                  sums[i] += matrix[i][right];
+              }
+
+              // We use TreeSet to help us find the rectangle with maxSum <= k with O(logN) time
+              TreeSet<Integer> set = new TreeSet<Integer>();
+              // Add 0 to cover the single row case
+              set.add(0);
+              int currSum = 0;
+
+              for (int sum : sums) {
+                  currSum += sum;
+                  // We use sum subtraction (curSum - sum) to get the subarray with sum <= k
+                  // Therefore we need to look for the smallest sum >= currSum - k
+                  Integer num = set.ceiling(currSum - k);
+                  if (num != null) result = Math.max( result, currSum - num );
+                  set.add(currSum);
+              }
+          }
+      }
+
+      return result;
+  }
+  /*
+  {{1,0,1},       {{ 1,  0, 1},
+  {0,-2,3}};       { 0, -1, 2}};
+
+  {{1,1,1,0},      {{1,2,3,0},
+  {1,1,1,0},        {2,4,6,0},
+  {1,1,1,0},        {3,6,9,0},
+  {0,0,0,0}};       {0,0,0,0}};
+
+  */
+  public static void main (String[] args) {
+
+    int k = 2;
+    int[][] array = {{1,0,1},
+                     {0,-2,3}};
+
+    // int[][] array = {{1,1,1,1},
+    //                  {1,1,1,1},
+    //                  {1,1,1,1},
+    //                  {1,1,1,1}};
+    print(array);
+    System.out.println(maxSumSubmatrix(array,2));
+  }
+}
--- Java/General/Chapter_4_TreesGraphs.java
+++ Java/General/Chapter_4_TreesGraphs.java
@@ -14,7 +14,140 @@ Questions
 
 class Chapter_4_TreesGraphs {
 
+  public static class TreeNode {
+    int data;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int e) {
+      this.data = e;
+    }
+  }
+
+
+  /*
+  2.InOrder Recursive Traversal
+  */
+  public static void inOrderRecursive(TreeNode n) {
+    if (n == null) return;
+
+    inOrderRecursive(n.left);
+    System.out.println(n.data);
+    inOrderRecursive(n.right);
+  }
+
+
+  // public static List<Integer> inorderIterative(TreeNode root) {
+  //     List<Integer> list = new ArrayList<Integer>();
+  //
+  //     Stack<TreeNode> stack = new Stack<TreeNode>();
+  //     TreeNode cur = root;
+  //
+  //     while(cur!=null || !stack.empty()){
+  //         while(cur!=null){
+  //             stack.add(cur);
+  //             cur = cur.left;
+  //         }
+  //         cur = stack.pop();
+  //         list.add(cur.val);
+  //         cur = cur.right;
+  //     }
+  //
+  //     return list;
+  // }
+
+  /*
+  2.PreOrder Recursive Traversal
+  */
+  public static void preOrderRecursive(TreeNode n) {
+    if (n == null) return;
+
+    System.out.println(n.data);
+    preOrderRecursive(n.left);
+    preOrderRecursive(n.right);
+  }
+
+  /*
+  2.PostOrder Recursive Traversal
+  */
+  public static void postOrderRecursive(TreeNode n) {
+    if (n == null) return;
+
+    postOrderRecursive(n.left);
+    postOrderRecursive(n.right);
+    System.out.println(n.data);
+  }
+
+
+  /*
+  3.Level Order BFS Iterative
+  */
+  public static void bfsIterative(TreeNode n) {
+    if (n == null) return;
+    Queue<TreeNode> queue = new LinkedList<TreeNode>();
+    queue.add(n);
+
+    while (!queue.isEmpty()) {
+      TreeNode temp = queue.remove();
+
+      System.out.println(temp.data);
+      if (temp.left != null) queue.add(temp.left);
+      if (temp.right != null) queue.add(temp.right);
+    }
+
+  }
+
+  /*
+  3.DFS Iterative
+  */
+  public static void dfsIterative(TreeNode n) {
+
+  }
+
+
+
+  /*
+            4
+          /   \
+         2     6
+        / \   / \
+       1  3  5  7
+                 \
+                  8
+
+  */
   public static void main (String[] args) {
-  
+    TreeNode n1 = new TreeNode(1);
+    TreeNode n2 = new TreeNode(2);
+    TreeNode n3 = new TreeNode(3);
+    TreeNode n4 = new TreeNode(4);
+    TreeNode n5 = new TreeNode(5);
+    TreeNode n6 = new TreeNode(6);
+    TreeNode n7 = new TreeNode(7);
+    TreeNode n8 = new TreeNode(8);
+
+    n4.left = n2;
+    n2.left = n1;
+    n2.right = n3;
+    n4.right = n6;
+    n6.left = n5;
+    n6.right = n7;
+    n7.right = n8;
+
+    System.out.println("2.InOrder Recursive");
+    inOrderRecursive(n4);
+    System.out.println("-");
+
+    System.out.println("2.PreOrder Recursive");
+    preOrderRecursive(n4);
+    System.out.println("-");
+
+    System.out.println("2.PostOrder Recursive");
+    postOrderRecursive(n4);
+    System.out.println("-");
+
+    System.out.println("3.Level Order BFS Iterative ");
+    bfsIterative(n4);
+    System.out.println("-");
+
   }
 }
