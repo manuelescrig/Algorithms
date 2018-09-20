3976dd7f6b6bc30fd3ea6feecc820dc49e0219e4
Manuel Escrig <manuelescrig@gmail.com>
2018-09-20 16:21:24 +0700

    Adds new algorithms.

---
+++ Swift/Algorithms/Implementation/Birthday Bar.swift
@@ -0,0 +1,42 @@
+//
+//  Birthday Bar.swift
+
+import UIKit
+
+// Complete the birthday function below.
+func birthday(s: [Int], d: Int, m: Int) -> Int {
+    var result = 0
+
+    for index in 0..<s.count {
+
+        if index + m <= s.count {
+            var currentSum = 0
+            var currentIndex = 0
+
+            for j in index..<index + m {
+                currentSum += s[j]
+                currentIndex += 1
+            }
+
+            if currentIndex == m && currentSum == d {
+                result += 1
+            }
+        }
+    }
+
+    return result
+}
+
+
+let s = [1, 2, 1, 3, 2]
+print(birthday(s: s, d: 3, m: 2))
+// Result = 2
+
+//let s = [1, 1, 1, 1, 1, 1]
+//print(birthday(s: s, d: 3, m: 2))
+// Result = 0
+
+//let s = [4]
+//print(birthday(s: s, d: 4, m: 1))
+// Result = 1
+
---
+++ Swift/Algorithms/Implementation/Grading Students.swift
@@ -0,0 +1,24 @@
+//
+//  Grading Students.swift
+//  
+//
+
+func gradingStudents(grades: [Int]) -> [Int] {
+    var results = grades
+
+    for index in 0..<grades.count {
+        let currentGrade = grades[index]
+        if currentGrade >= 38 {
+
+            let nextMultiple = currentGrade + (5 - currentGrade % 5)
+            if nextMultiple - currentGrade < 3 {
+                results[index] = nextMultiple
+            }
+        }
+    }
+
+    return results
+}
+
+let grates = [73, 67, 38, 33]
+print(gradingStudents(grades: grates))
