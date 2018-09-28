4f2231c11d1372cbe60911946cd99e6a88315fcf
Manuel Escrig <manuelescrig@gmail.com>
2018-09-28 12:09:20 +0700

    Adds project.

---
+++ Swift/Project/Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Manuel Escrig on 28/9/18.
+//  Copyright © 2018 Manuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
