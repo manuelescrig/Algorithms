14fa5b944680be30c1f1aa16b509c65128a1570a
Manuel Escrig <manuelescrig@gmail.com>
2020-01-22 18:15:41 +0700

    Updates Median of two arrays problem.

--- Java/ByteByByte/MedianOfArrays.java
+++ Java/ByteByByte/MedianOfArrays.java
@@ -8,25 +8,66 @@ arr2 = [​2​, ​4​, ​6​]
 median(arr1, arr2) = ​3.5
 */
 
-class MedianOfArrays {
-
-  public static String reverseWords(String s) {
-    if (s == null) throw new IllegalStateException("Invalid input.");
-    if (s.length() == 0) return s;
-
-    StringBuilder sb = new StringBuilder("");
-    for(int i = s.length() - 1; i >= 0; i--) {
-      if(Character.isWhitespace(s.charAt(i))) {
-        sb.append(s.substring(i + 1, s.length() - sb.length())).append(" ");;
-      }
-    }
-
-    sb.append(s.substring(0, s.length() - sb.length()));
-    return sb.toString();
-  }
-
-  public static void main (String[] args) {
-    String s = "the sky is blue";
-    System.out.println(reverseWords(s));
-  }
-}
+// A Simple Merge based O(n) solution  
+// to find median of two sorted arrays 
+  
+class MedianOfArrays  { 
+    
+    // Function to calculate median 
+    static int getMedian(int ar1[], int ar2[], int n) {    
+        int i = 0;   
+        int j = 0;  
+        int count; 
+        int m1 = -1, m2 = -1; 
+       
+        /* Since there are 2n elements, median will  
+           be average of elements at index n-1 and  
+           n in the array obtained after merging ar1  
+           and ar2 */
+        for (count = 0; count <= n; count++) { 
+            /* Below is to handle case where all  
+              elements of ar1[] are smaller than  
+              smallest(or first) element of ar2[] */
+            if (i == n) { 
+                m1 = m2; 
+                m2 = ar2[0]; 
+                break; 
+            } else if (j == n) { 
+              /* Below is to handle case where all  
+               elements of ar2[] are smaller than  
+               smallest(or first) element of ar1[] */
+                m1 = m2; 
+                m2 = ar1[0]; 
+                break; 
+            } 
+       
+            if (ar1[i] < ar2[j]) {    
+                /* Store the prev median */
+                m1 = m2;   
+                m2 = ar1[i]; 
+                i++; 
+            } else { 
+                /* Store the prev median */
+                m1 = m2;   
+                m2 = ar2[j]; 
+                j++; 
+            } 
+        } 
+       
+        return (m1 + m2)/2; 
+    } 
+       
+    /* Driver program to test above function */
+    public static void main (String[] args) { 
+        int ar1[] = {1, 12, 15, 26, 38}; 
+        int ar2[] = {2, 13, 17, 30, 45}; 
+       
+        int n1 = ar1.length; 
+        int n2 = ar2.length; 
+        if (n1 == n2) {
+            System.out.println("Median is " + getMedian(ar1, ar2, n1)); 
+        } else {
+            System.out.println("arrays are of unequal size"); 
+        }
+    }     
+} 
